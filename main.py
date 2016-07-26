e46f4782e097d01861e50ca0c94bedb82453c398
Ubuntu <mraza@hn0-pyspar.51jcoh2wtz5u1ndg4zpvfn3fgg.bx.internal.cloudapp.net>
2016-07-26 22:51:15 +0000

    minor

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ if __name__=='__main__':
 	parser.add_argument('-of','--output_file',help='Output File', required=True)
 
 	args=parser.parse_args()
-	df=gl.SFrame.read_csv(parser.input_file, delimiter='\t', header=None)
+	df=gl.SFrame.read_csv(args.input_file, delimiter='\t', header=None)
 
 	df.rename({'X1':'tID','X2':'eName','X3':'eType','X4':'tText','X5':'tUrl','X6':'tTitleForUrl','X7':'tHashtags','X8':'tTotalRetweetCount','X9':'tIsRetweet','X10':'tOriginalID','X11':'tPostTime','X12':'tUtcOffset','X13':'tReceivedTime','X14':'tAdultScore','X15':'tQualityScore','X16':'tAuthorityScore','X17':'uScreenName','X18':'uFollowersCount','X19':'uIsVerified','X20':'tWordBrokenHashtag','X21':'tGeoPoint','X22':'uLocation','X23':'uTimeZone','X24':'tFavoriteCount','X25':'tReceivedHour'})
 
 	
 	df_loc=filter_locations(df)
 	dfEng=detectLanguage(df_loc)
-	dfEng.export_csv(parser.output_file, quote_level=csv.QUOTE_NONE)
+	dfEng.export_csv(args.output_file, quote_level=csv.QUOTE_NONE, delimiter='\t')
 	
